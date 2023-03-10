7e71ab1745737b5fe03854b3aa2ba0aef16c5c66
Krisztian Korosi <koeroesi86@gmail.com>
2023-03-10 21:15:27 +0100

    Attempt to fix publish

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,12 @@ const typescript = require('@rollup/plugin-typescript');
 
 /** @type {import('rollup').RollupOptions} */
 module.exports = {
-  input: ['./src/index.ts','./src/Worker.ts'],
+  input: ['./src/index.ts', './src/Worker.ts'],
   output: {
     dir: './dist',
     format: 'cjs',
     sourcemap: true,
   },
-  plugins: [typescript()]
+  external: ['child_process'],
+  plugins: [typescript()],
 };
