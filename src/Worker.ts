5b75763ee80884def6eb9af4a0f1c31e68c88362
Krisztian Korosi <Koeroesi86@users.noreply.github.com>
2023-03-11 17:07:53 +0100

    Eslint setup (#2)
    * Update signatures
    
    * Eslint setup with cache
    
    * npmignore config

--- src/Worker.ts
+++ src/Worker.ts
@@ -6,39 +6,32 @@ export default class Worker implements EventTarget {
 
   constructor(command: string, options: Parameters<typeof spawn>[2] = {}) {
     this.commandParts = command.split(' ');
-    this.instance = spawn(
-      this.commandParts[0],
-      [
-        ...this.commandParts.slice(1)
-      ],
-      options
-    );
+    this.instance = spawn(this.commandParts[0], [...this.commandParts.slice(1)], options);
     this.instance.once('close', () => {
       this.instance = null;
       delete this.instance;
     });
   }
 
-
-  set onmessage(onmessage: () => unknown) {
+  set onmessage(onmessage: (...args: unknown[]) => unknown) {
     this.addEventListener('message', onmessage);
   }
 
-  set onerror(onerror: () => unknown) {
+  set onerror(onerror: (...args: unknown[]) => unknown) {
     this.addEventListener('error', onerror);
   }
 
-  addEventListener = (event: string, listener: () => unknown) => {
+  addEventListener = (event: string, listener: (...args: unknown[]) => unknown) => {
     if (this.instance) this.instance.on(event, listener);
-  }
+  };
 
-  addEventListenerOnce = (event: string, listener: () => unknown) => {
+  addEventListenerOnce = (event: string, listener: (...args: unknown[]) => unknown) => {
     if (this.instance) this.instance.once(event, listener);
-  }
+  };
 
-  removeEventListener = (event: string, listener: () => unknown) => {
+  removeEventListener = (event: string, listener: (...args: unknown[]) => unknown) => {
     if (this.instance && this.instance.off) this.instance.off(event, listener);
-  }
+  };
 
   dispatchEvent = ({ type, payload = '' }: Event & { payload?: Serializable }) => {
     if (this.instance && this.instance.emit) {
@@ -46,13 +39,13 @@ export default class Worker implements EventTarget {
     }
 
     return false;
-  }
+  };
 
   terminate = () => {
     if (this.instance) this.instance.kill('SIGINT');
-  }
+  };
 
   postMessage = (message: Serializable, cb: (error: Error | null) => void = () => {}) => {
     if (this.instance && this.instance.send) this.instance.send(message, cb);
-  }
+  };
 }
