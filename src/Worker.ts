e804c79ffc1fa632c1500eebd3297cac9f17de13
Krisztian Korosi <koeroesi86@gmail.com>
2023-03-11 16:59:57 +0100

    Fix eslint config

--- src/Worker.ts
+++ src/Worker.ts
@@ -6,19 +6,12 @@ export default class Worker implements EventTarget {
 
   constructor(command: string, options: Parameters<typeof spawn>[2] = {}) {
     this.commandParts = command.split(' ');
-    this.instance = spawn(
-      this.commandParts[0],
-      [
-        ...this.commandParts.slice(1)
-      ],
-      options
-    );
+    this.instance = spawn(this.commandParts[0], [...this.commandParts.slice(1)], options);
     this.instance.once('close', () => {
       this.instance = null;
       delete this.instance;
     });
   }
-
 
   set onmessage(onmessage: (...args: unknown[]) => unknown) {
     this.addEventListener('message', onmessage);
@@ -30,15 +23,15 @@ export default class Worker implements EventTarget {
 
   addEventListener = (event: string, listener: (...args: unknown[]) => unknown) => {
     if (this.instance) this.instance.on(event, listener);
-  }
+  };
 
   addEventListenerOnce = (event: string, listener: (...args: unknown[]) => unknown) => {
     if (this.instance) this.instance.once(event, listener);
-  }
+  };
 
   removeEventListener = (event: string, listener: (...args: unknown[]) => unknown) => {
     if (this.instance && this.instance.off) this.instance.off(event, listener);
-  }
+  };
 
   dispatchEvent = ({ type, payload = '' }: Event & { payload?: Serializable }) => {
     if (this.instance && this.instance.emit) {
@@ -46,13 +39,13 @@ export default class Worker implements EventTarget {
     }
 
     return false;
-  }
+  };
 
   terminate = () => {
     if (this.instance) this.instance.kill('SIGINT');
-  }
+  };
 
   postMessage = (message: Serializable, cb: (error: Error | null) => void = () => {}) => {
     if (this.instance && this.instance.send) this.instance.send(message, cb);
-  }
+  };
 }
